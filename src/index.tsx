9a583398562d38b6a2df4e9ef5b02609049f7019
grasielarosa <grasielarosaa@gmail.com>
2021-12-29 12:13:55 +0100

    welcome page started

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App';
+import { Welcome } from './pages';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Welcome />
   </React.StrictMode>,
   document.getElementById('root')
 );
