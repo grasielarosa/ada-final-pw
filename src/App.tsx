63e9eaf272c26d1221fdd80d683fa17a2a24c023
grasielarosa <grasielarosaa@gmail.com>
2021-12-29 13:20:25 +0100

    bootstrap install

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { Login, SignUp, Welcome } from './pages';
 
 
