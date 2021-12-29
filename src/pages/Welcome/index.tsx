95e2afe62ccd0b5d8053929404bc64048d31c02e
grasielarosa <grasielarosaa@gmail.com>
2021-12-29 13:00:30 +0100

    test with link

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,25 +1,15 @@
 import React, { FC } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Welcome: FC = () => {
-    const { push } = useHistory();
 
-    const handleLogin = () => {
-        push('/login');
-    }
-
-    const handleSignup = () => {
-        push('/sign-up');
-
-    }
 
     return (
         <>
             <h3>Welcome to</h3>
             <h1>CinemAda</h1>
-            <button onClick={handleLogin}>Login</button>
-            <button onClick={handleSignup}>SignUp</button>
-
+            <Link to='/login'>Login</Link>
+            <Link to='/sign-up'>SignUp</Link>
         </>
     )
 };
