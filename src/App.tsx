e92791e869ac2f4f358f68dc72f03f18dadb8c42
grasielarosa <grasielarosaa@gmail.com>
2021-12-30 13:47:50 +0100

    global styles defined and colors test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/GlobalStyles.css';
 
 import {
   Admin,
