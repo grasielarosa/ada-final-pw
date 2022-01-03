6d94651d339763c1beaecb045bdae1f4a7ae7b24
grasielarosa <grasielarosaa@gmail.com>
2022-01-03 16:38:18 +0100

    test

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,8 +3,6 @@ import { getDataMovies } from '../../api/tmdb';
 import { getDataUsers } from '../../api/myApi';
 
 const Login = () => {
-  getDataMovies();
-  getDataUsers();
   return <div>hello, world</div>;
 };
 
