6fd0b86f648adc63848e619b7d035c74b5cd0b78
grasielarosa <grasielarosaa@gmail.com>
2022-01-22 11:51:47 +0100

    add user data media function

--- src/pages/Movies/api.ts
+++ src/pages/Movies/api.ts
@@ -1,1 +1,11 @@
-export {};
+import { Data, User } from '../../types';
+import { myApi } from '../../utils';
+
+const postUserDataMedia = async (
+  currentUser: Partial<User | undefined>,
+  movie: Data
+): Promise<void> => {
+  await myApi.post(`users/${currentUser?.id}/data-media.json`, movie);
+};
+
+export { postUserDataMedia };
