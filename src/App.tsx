9f272dbdf91f9c0d6aa7a116c91e2723bd4a1d4c
grasielarosa <grasielarosaa@gmail.com>
2022-01-22 11:26:19 +0100

    added currentUser on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,21 +15,27 @@ import {
   Users,
   Welcome,
 } from './pages';
-import { AuthProvider } from './context/Auth';
+import { AuthContext, AuthProvider } from './context/Auth';
 
 const App: FC = () => {
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <AuthProvider>
       <Router>
         <Switch>
-          <Route path="/admin" component={Admin} />
-          <Route path="/detail" component={Detail} />
+          {currentUser?.role === 'admin' && (
+            <Route path="/admin" component={Admin} />
+          )}
+          {currentUser?.role === 'admin' && (
+            <Route path="/users" component={Users} />
+          )}
+          <Route path="/home" component={Home} />
           <Route path="/movies" component={Movies} />
           <Route path="/series" component={Series} />
-          <Route path="/users" component={Users} />
+          <Route path="/detail" component={Detail} />
           <Route path="/login" component={Login} />
           <Route path="/sign-up" component={SignUp} />
-          <Route path="/home" component={Home} />
           <Route path="/" component={Welcome} />
         </Switch>
       </Router>
