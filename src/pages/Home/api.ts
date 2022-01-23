371442322e43a02a5d34ed9d542c39030b1a7d4b
grasielarosa <grasielarosaa@gmail.com>
2022-01-23 19:52:44 +0100

    deleted no used function

--- src/pages/Home/api.ts
+++ src/pages/Home/api.ts
@@ -2,13 +2,8 @@ import { mapToArray } from '../../helpers';
 import { User } from '../../types';
 import { myApi } from '../../utils';
 
-// const getUser = async (id: string): Promise<User> => {
-//   const response = await myApi.get<User>(`users/${id}.json`);
-//   return response.data;
-// };
-
 const getUserDataMedia = async (currentUser: Partial<User | undefined>) => {
   const response = await myApi.get(`users/${currentUser?.id}/data-media.json`);
   return mapToArray(response.data);
 };
-export { /* getUser */ getUserDataMedia };
+export { getUserDataMedia };
