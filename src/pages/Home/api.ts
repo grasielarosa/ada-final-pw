f75c47fc0ad54314eca4046aae0447fb7d29225c
grasielarosa <grasielarosaa@gmail.com>
2022-02-01 19:28:01 +0100

    move functions from api to  myapi

--- src/pages/Home/api.ts
+++
@@ -1,9 +0,0 @@
-import { mapToArray } from '../../helpers';
-import { User } from '../../types';
-import { myApi } from '../../utils';
-
-const getUserDataMedia = async (currentUser: Partial<User | undefined>) => {
-  const response = await myApi.get(`users/${currentUser?.id}/data-media.json`);
-  return mapToArray(response.data);
-};
-export { getUserDataMedia };
