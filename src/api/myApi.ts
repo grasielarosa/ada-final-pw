cd0579f70d462d200bbd8a341fd9093e1a9c32c5
grasielarosa <grasielarosaa@gmail.com>
2022-02-01 19:28:22 +0100

    move functions from api to  myapi

--- src/api/myApi.ts
+++ src/api/myApi.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { mapToArray } from '../helpers';
-import { Data } from '../types';
+import { Data, User } from '../types';
 import { myApi } from '../utils';
 
 const getDataMedia = async () => {
@@ -23,4 +23,34 @@ const deleteDataMedia = async (movie: Data): Promise<void> => {
   await myApi.delete(`data-media/${deleteData.idDB}.json`);
 };
 
-export { deleteDataMedia, getDataUsers, getDataMedia, postDataMedia };
+const postUserDataMedia = async (
+  currentUser: Partial<User | undefined>,
+  movie: Data
+): Promise<void> => {
+  await myApi.post(`users/${currentUser?.id}/data-media.json`, movie);
+};
+
+const getUserDataMedia = async (currentUser: Partial<User | undefined>) => {
+  const response = await myApi.get(`users/${currentUser?.id}/data-media.json`);
+  return mapToArray(response.data);
+};
+const deleteUserDataMedia = async (
+  currentUser: Partial<User | undefined>,
+  movie: Data
+): Promise<void> => {
+  const data = await getUserDataMedia(currentUser);
+  const deleteData = data.find((item) => item.id === movie.id);
+  await myApi.delete(
+    `users/${currentUser?.id}/data-media/${deleteData.idDB}.json`
+  );
+};
+
+export {
+  deleteDataMedia,
+  deleteUserDataMedia,
+  getDataUsers,
+  getDataMedia,
+  getUserDataMedia,
+  postDataMedia,
+  postUserDataMedia,
+};
