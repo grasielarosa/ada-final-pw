496cf7fb95af68c197452a1a3bcccd38fd214c1d
grasielarosa <grasielarosaa@gmail.com>
2022-02-02 18:29:19 +0100

    added functions and styles to detail page

--- src/pages/Detail/api.ts
+++ src/pages/Detail/api.ts
@@ -1,9 +1,24 @@
 import { mapToArray } from '../../helpers';
-import { myApi } from '../../utils';
-import { Data } from '../../types';
+import { myApi, theMovieApi } from '../../utils';
+import { Data, Movies } from '../../types';
 
-const getDataMediaDetail = async (): Promise<Data[]> => {
+const getDataMediaDetail = async () => {
   const response = await myApi.get<Data>(`data-media.json`);
   return mapToArray(response.data);
 };
-export { getDataMediaDetail };
+
+const getDataMediaDetail2 = async () => {
+  const response = await myApi.get<Data>(`data-media.json`);
+  return response.data;
+};
+
+const getVideos = async (id: number) => {
+  const response = await theMovieApi.get(`/movie/${id}/videos`);
+  return response.data;
+};
+const getSeries = async (id: number) => {
+  const response = await theMovieApi.get(`/tv/${id}/videos`);
+  return response.data.results;
+};
+
+export { getDataMediaDetail, getSeries, getVideos };
