91aba825c82ff690efb7986a79340034c7c7bb5a
grasielarosa <grasielarosaa@gmail.com>
2022-02-04 18:14:57 +0100

    created home page to admin

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { CardGroup, Container, Pagination, UsersTable } from './commons';
 export { SignUp, Login } from './forms';
 export { Header, Footer, Wrapper } from './layout';
+export { Admin } from './parts';
