b284bf422aca8375adb9f0429062309ca5ccbb61
grasielarosa <grasielarosaa@gmail.com>
2022-02-04 18:14:22 +0100

    created home page to admin

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { CardGroup, Wrapper } from '../../components';
+import { Admin, CardGroup, Wrapper } from '../../components';
 import { withAuth } from '../../hoc';
 import { useUsers } from '../../hooks/useUsers';
 
 const HomePage = () => {
-  const { data, handleButton } = useUsers();
+  const { handleButtonWatch, currentUser, data } = useUsers();
 
   return (
-    <Wrapper hideFooter>
-      <CardGroup handleButton={handleButton} items={data} />
+    <Wrapper>
+      {currentUser?.role !== 'admin' && (
+        <CardGroup handleButton={handleButtonWatch} items={data} />
+      )}
+      {currentUser?.role === 'admin' && <Admin />}
     </Wrapper>
   );
 };
